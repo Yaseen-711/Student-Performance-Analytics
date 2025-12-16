62d097d1d93de60d6bd862cb246f7c6d617dd01a
Yaseen-711 <yaseen0706@gmail.com>
2025-12-16 23:01:42 +0530

    initial commit

--- data.py
+++ data.py
@@ -7,15 +7,15 @@ import seaborn as sns
 df = pd.read_csv("D:/PROJECTS/programs/programs/student performance analytics/student_data.csv")
 
 #clean the dataframe
-#convert all invalid entry to NaN
-cols = df.columns.difference(['Student ID'])
-df[cols] = df[cols].apply(pd.to_numeric, errors='coerce')
 
-marks_cols = ['Maths', 'English', 'Chemistry',
-              'Electronics', 'Python', 'Physics']
+#convert all invalid entry to NaN
+marks_cols = ['Maths', 'English', 'Chemistry','Electronics', 'Python', 'Physics']
+lifestyle_cols = ['sleep_hours', 'screen_time']
+df[marks_cols + lifestyle_cols] = df[marks_cols + lifestyle_cols].apply(pd.to_numeric, errors='coerce')
 
 # remove rows where ALL subject marks are NaN
 df = df.dropna(subset=marks_cols, how='all')
+
 #fill missing values
 df[marks_cols] = df[marks_cols].fillna(0)
 df['sleep_hours'] = df['sleep_hours'].fillna(df['sleep_hours'].median())
@@ -27,8 +27,10 @@ df = df.drop_duplicates(subset=['Student ID'])
 # each subject 50 max
 df[marks_cols] = df[marks_cols].clip(0, 50)
 
-# average of 6 subjects, scaled to 100
-df['percentage'] = (df[marks_cols].mean(axis=1) / 50) * 100
+# average of 6 subjects, scaled to 100  , Total of all subjects
+df['total_marks'] = df[marks_cols].sum(axis=1)
+
 
 
 print(df.describe())
+print(df.dtypes)
